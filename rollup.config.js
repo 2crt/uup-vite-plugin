f85082f6cf85a6c79cc1601e19b463cfab550ed1
Emil Mohamed <emil@2create.studio>
2024-04-12 18:33:22 +0300

    Fix build

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,20 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
 export default {
-	plugins: [
-		commonjs(),
-		nodeResolve()
-	]
+    input: 'index.js',
+    output: [
+        {
+            file: 'dist/bundle.cjs.js',
+            format: 'cjs',
+            exports: 'named',
+        },
+        {
+            file: 'dist/bundle.esm.js',
+            format: 'esm',
+        },
+    ],
+    plugins: [
+        commonjs(),
+        nodeResolve()
+    ]
 };
